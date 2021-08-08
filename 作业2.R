bdb81f6a0d60fc8ef6d1f015de45ddb37058a026
xzlczy <88613371+xzlczy@users.noreply.github.com>
2021-08-08 18:23:46 +0800

    Add files via upload

---
+++ 作业2.R
@@ -0,0 +1,49 @@
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+        inmatx <- x$getinvm()
+        if(!is.null(inmatx)){
+                message("getting cached data")
+                return(inmatx)
+        }
+        data <- x$get()
+        inmatx <- solve(data,...)
+        x$setinm(inmatx)
+        inmatx
+        
+}
+CacheMatrix <- function(x = matrix()) {
+        inmatx <-null
+        set <-function(y){
+                x <<- y
+                inmatx <<- null
+        }
+        get<-function() x
+        setinvm <- function(solve) inmatx <<- solve
+        getinvm <- function() inmatx
+        
+        list(set=set,get=get,
+             setinvm=setinvm,
+             getinvm=getinvm)
+        
+}
+
+
+## Write a short comment describing this function
+## cacheSolve function first check whether the inverse of the matrix has been calculated
+## if yes, the function will cache it and return it.
+## if not, the function will calculate it.
+
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+        inmatx <- x$getinvm()
+        if(!is.null(inmatx)){
+                message("getting cached data")
+                return(inmatx)
+        }
+        data <- x$get()
+        inmatx <- solve(data,...)
+        x$setinm(inmatx)
+        inmatx
+        
+}
